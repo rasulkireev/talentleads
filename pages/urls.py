f79b4c5cf55f8556a91f922d8b5cf2178a04e534
Rasul Kireev <rasul.kireev@guycarp.com>
2023-05-23 02:40:15 -0400

    page is ready

--- pages/urls.py
+++ pages/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
 
-from .views import HomeView, PricingView
+from .views import HomeView, PricingView, ProductHuntView
 
 urlpatterns = [
     path("", HomeView.as_view(), name="home"),
+    path("product-hunt", ProductHuntView.as_view(), name="product-hunt"),
     path("pricing", PricingView.as_view(), name="pricing"),
 ]
