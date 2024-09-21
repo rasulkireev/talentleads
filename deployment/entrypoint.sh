72877e48d9adc7cea9c57d38934d9844522ab98f
Rasul Kireev <me@rasulkireev.com>
2024-09-21 22:28:54 +0300

    catch connection errors

--- deployment/entrypoint.sh
+++ deployment/entrypoint.sh
@@ -38,7 +38,7 @@ if [ "$server" = true ]; then
     # python manage.py djstripe_sync_models
     export OTEL_SERVICE_NAME=${PROJECT_NAME}_${ENVIRONMENT:-dev}
     export OTEL_RESOURCE_ATTRIBUTES=service.name=${PROJECT_NAME}_${ENVIRONMENT:-dev}
-    opentelemetry-instrument gunicorn ${PROJECT_NAME}.wsgi:application -c deployment/gunicorn.config.py --bind 0.0.0.0:80 --workers 3 --threads 2 --reload
+    gunicorn ${PROJECT_NAME}.wsgi:application -c deployment/gunicorn.config.py --bind 0.0.0.0:80 --workers 3 --threads 2 --reload
 else
     export OTEL_SERVICE_NAME="${PROJECT_NAME}_${ENVIRONMENT:-dev}_workers"
     export OTEL_RESOURCE_ATTRIBUTES=service.name=${PROJECT_NAME}_${ENVIRONMENT:-dev}_workers
--- deployment/gunicorn.config.py
+++ deployment/gunicorn.config.py
@@ -5,6 +5,7 @@ from opentelemetry.exporter.otlp.proto.http.trace_exporter import OTLPSpanExport
 from opentelemetry.sdk.resources import Resource
 from opentelemetry.sdk.trace import TracerProvider
 from opentelemetry.sdk.trace.export import BatchSpanProcessor
+from requests.exceptions import ConnectionError, RequestException, SSLError
 
 
 def post_fork(server, worker):
@@ -13,7 +14,10 @@ def post_fork(server, worker):
     resource = Resource.create(attributes={"service.name": os.environ.get("OTEL_SERVICE_NAME")})
 
     trace.set_tracer_provider(TracerProvider(resource=resource))
-    span_processor = BatchSpanProcessor(
-        OTLPSpanExporter(endpoint=f"{os.environ.get('OTEL_EXPORTER_OTLP_ENDPOINT')}/v1/traces")
-    )
-    trace.get_tracer_provider().add_span_processor(span_processor)
+    try:
+        span_processor = BatchSpanProcessor(
+            OTLPSpanExporter(endpoint=f"{os.environ.get('OTEL_EXPORTER_OTLP_ENDPOINT')}/v1/traces")
+        )
+        trace.get_tracer_provider().add_span_processor(span_processor)
+    except (RequestException, ConnectionError, SSLError):
+        pass
