950011f5cac1a7478c3d6b26e5458c9321357420
Rasul Kireev <me@rasulkireev.com>
2025-10-07 17:00:36 +0300

    remove-otel

--- deployment/entrypoint.sh
+++ deployment/entrypoint.sh
@@ -11,12 +11,6 @@ fi
 # All commands before the conditional ones
 export PROJECT_NAME=talentleads
 export DJANGO_SETTINGS_MODULE=${PROJECT_NAME}.settings
-export OTEL_EXPORTER_OTLP_ENDPOINT=https://signoz-otel-collector-proxy.cr.lvtd.dev
-export OTEL_EXPORTER_OTLP_PROTOCOL=http/protobuf
-
-python manage.py collectstatic --noinput
-python manage.py migrate
-python manage.py createschedules
 
 while getopts ":sw" option; do
     case "${option}" in
@@ -36,11 +30,11 @@ shift $((OPTIND - 1))
 # If no valid option provided, default to server
 if [ "$server" = true ]; then
     # python manage.py djstripe_sync_models
-    export OTEL_SERVICE_NAME=${PROJECT_NAME}_${ENVIRONMENT:-dev}
-    export OTEL_RESOURCE_ATTRIBUTES=service.name=${PROJECT_NAME}_${ENVIRONMENT:-dev}
-    opentelemetry-instrument gunicorn ${PROJECT_NAME}.wsgi:application --bind 0.0.0.0:80 --workers 3 --threads 2 --reload
+    python manage.py collectstatic --noinput
+    python manage.py migrate
+    python manage.py createschedules
+
+    gunicorn ${PROJECT_NAME}.wsgi:application --bind 0.0.0.0:80 --workers 3 --threads 2 --reload
 else
-    export OTEL_SERVICE_NAME="${PROJECT_NAME}_${ENVIRONMENT:-dev}_workers"
-    export OTEL_RESOURCE_ATTRIBUTES=service.name=${PROJECT_NAME}_${ENVIRONMENT:-dev}_workers
-    opentelemetry-instrument python manage.py qcluster
+    python manage.py qcluster
 fi
